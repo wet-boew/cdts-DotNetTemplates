b6608f82d724dea0845896950e96ada72ff51e41
CalvinRodo <calvinv@gmail.com>
2017-06-29 14:06:26 -0400

    Update automate script to include XSD and CDTSEnvironments.json Copy changes into Start GCPedia projects Closes #82

--- Builds/Batch Files/Automate.bat
+++ Builds/Batch Files/Automate.bat
@@ -1,8 +1,18 @@
+@echo off
+echo "Copy Static View Files"
 copy c:\workspaces\DotNetTemplates\SampleCode\SampleCode.C3\GoC.WebTemplate c:\workspaces\DotNetTemplates\Applications\GoCWebTemplateStarterASPX\GoC.WebTemplate /Y
 copy c:\workspaces\DotNetTemplates\SampleCode\SampleCode.C3.MVC\Views\GoC.WebTemplate c:\workspaces\DotNetTemplates\Applications\GoCWebTemplateStarterMVC\Views\GoC.WebTemplate /Y
+echo "Copy web.configs"
 copy c:\workspaces\DotNetTemplates\SampleCode\SampleCode.C3\Web.config c:\workspaces\DotNetTemplates\Applications\GoCWebTemplateStarterASPX\Web.config /Y
 copy c:\workspaces\DotNetTemplates\SampleCode\SampleCode.C3.MVC\Web.config c:\workspaces\DotNetTemplates\Applications\GoCWebTemplateStarterMVC\Web.config /Y
+echo "copy dlls"
 copy c:\workspaces\DotNetTemplates\SampleCode\SampleCode.C3\bin\GoC.WebTemplate.dll c:\workspaces\DotNetTemplates\Applications\GoCWebTemplateStarterASPX\Libraries\ /Y
 copy c:\workspaces\DotNetTemplates\SampleCode\SampleCode.C3\bin\WebTemplateCore.dll c:\workspaces\DotNetTemplates\Applications\GoCWebTemplateStarterASPX\Libraries\ /Y
 copy c:\workspaces\DotNetTemplates\SampleCode\SampleCode.C3.MVC\bin\WebTemplateCore.dll c:\workspaces\DotNetTemplates\Applications\GoCWebTemplateStarterMVC\Libraries\ /Y
 copy c:\workspaces\DotNetTemplates\SampleCode\SampleCode.C3.MVC\bin\GoC.WebTemplateMVC.dll c:\workspaces\DotNetTemplates\Applications\GoCWebTemplateStarterMVC\Libraries /Y
+echo "Copy CDTSEnvironments.json files"
+copy c:\workspaces\DotNetTemplates\SampleCode\SampleCode.C3\CDTSEnvironments.json c:\workspaces\DotNetTemplates\Applications\GoCWebTemplateStarterASPX\ /Y
+copy c:\workspaces\DotNetTemplates\SampleCode\SampleCode.C3.MVC\CDTSEnvironments.json c:\workspaces\DotNetTemplates\Applications\GoCWebTemplateStarterMVC\ /Y
+echo "Copy XSD"
+copy c:\workspaces\DotNetTemplates\SampleCode\SampleCode.C3\WebTemplateWebForms.xsd c:\workspaces\DotNetTemplates\Applications\GoCWebTemplateStarterASPX\ /Y
+copy c:\workspaces\DotNetTemplates\SampleCode\SampleCode.C3.MVC\WebTemplateMVC.xsd c:\workspaces\DotNetTemplates\Applications\GoCWebTemplateStarterMVC\ /Y
