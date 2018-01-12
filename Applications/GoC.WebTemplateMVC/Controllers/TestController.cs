56fbae72829948630e81bf0683e4472bbcc49af4
Cal <calvin.rodo@hrsdc-rhdcc.gc.ca>
2018-01-12 16:08:48 -0500

    Bilingual Page for MVC

---
+++ Applications/GoC.WebTemplateMVC/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using GoC.WebTemplate;
+
+namespace GoC.WebTemplateMVC.Controllers
+{
+    public class TestController : WebTemplateBaseController
+    {
+
+
+        public ActionResult BilingualErrorPage()
+        {
+            WebTemplateCore.HeaderTitle = "English | Francais";
+            return View();
+        }
+    }
+}
