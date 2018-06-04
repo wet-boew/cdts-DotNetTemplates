e258bfa1a59ff5dd7f4b19877f8bf82c8dc8d7bf
josee.n.paquette <josee.n.paquette@hrsdc-rhdcc.gc.ca>
2018-06-04 13:32:37 -0400

    fixing RenderLeftMenu code not compiling.

--- CoreTest/RenderTests/RenderTests.cs
+++ CoreTest/RenderTests/RenderTests.cs
@@ -77,7 +77,7 @@ namespace CoreTest.RenderTests
 
             var result = sut.RenderLeftMenu();
 
-            result.ToString().Should().Be("{\"sections\":[{\"sectionName\":\"SectionName\",\"sectionLink\":\"SectionLink\",\"newWindow\":false,\"menuLinks\":[{\"href\":\"Href\",\"text\":\"Text\"},{\"href\":\"Href\",\"text\":\"Text\",\"newWindow\":false,\"subLinks\":[{\"subHref\":\"subHerf\",\"subText\":\"subText\",\"newWindow\":false}]},{\"href\":\"Herf\",\"text\":\"Text\",\"newWindow\":true}]}]}");
+            result.ToString().Should().Be("{\"sections\":[{\"sectionName\":\"SectionName\",\"sectionLink\":\"SectionLink\",\"menuLinks\":[{\"href\":\"Href\",\"text\":\"Text\"},{\"href\":\"Href\",\"text\":\"Text\",\"subLinks\":[{\"subHref\":\"subHerf\",\"subText\":\"subText\"}]},{\"href\":\"Herf\",\"text\":\"Text\",\"newWindow\":true}]}]}");
         }
         
         [Theory, AutoNSubstituteData]
