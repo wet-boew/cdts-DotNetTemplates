9c3d7dbab4b0b81056e9aa95235388208e6a054e
a.shahid <ahmad.shahid@hrsdc-rhdcc.gc.ca>
2018-11-21 17:12:58 -0500

    Updated the contact links

--- SampleCode/GoC.WebTemplate-WebForms.Sample/SamplePages/FooterLinksSample.aspx.cs
+++ SampleCode/GoC.WebTemplate-WebForms.Sample/SamplePages/FooterLinksSample.aspx.cs
@@ -10,7 +10,16 @@ namespace GoC.WebTemplate.WebForm.Sample.SamplePages
         protected void Page_Load(object sender, EventArgs e)
         {
             //Contact Links
-            WebTemplateMaster.WebTemplateCore.ContactLinks = new List<Link> { new Link("http://travel.gc.ca/", "Contact Us") };
+            WebTemplateMaster.WebTemplateCore.ContactLinks = new List<Link> { new Link { Href = "http://travel.gc.ca/" } };
+
+            //The code snippet below displays an example of multiple links that have text and href being updated. 
+            /*
+                WebTemplateMaster.WebTemplateCore.ContactLinks = new List<Link> 
+                { 
+                    new Link { Href = "http://travel.gc.ca/", Text = "Contact Now"}, 
+                    new Link { Href = "http://travel.gc.ca/", Text = "Contact Info"} 
+                };
+            */
 
             //Note: For your solution, the values should be coming from your culture sensitive source ex: resource files, db etc...)
         }
