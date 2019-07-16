28554536bff703d76afd5708b95d6cedc618ea8e
s.laugh <shaun.laughland@hrsdc-rhdcc.gc.ca>
2019-07-16 12:30:17 -0400

    adding and moving projects for core adoption

---
+++ GoC.WebTemplate.Components/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GoC.WebTemplate.Components
+{
+    public class Class1
+    {
+    }
+}
---
+++ GoC.WebTemplate.CoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoC.WebTemplate.CoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
