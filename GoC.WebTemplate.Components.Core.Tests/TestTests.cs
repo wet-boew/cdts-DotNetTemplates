a9be3e0b017a34027d10d34efd9176ad46af24d1
Shaun Laughland <shaun.laughland@hrsdc-rhdcc.gc.ca>
2019-08-09 13:57:36 -0400

    Build updates

---
+++ GoC.WebTemplate.Components.Core.Tests/TestTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace GoC.WebTemplate.Components.Core.Tests
+{
+    public class TestTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            var num = 4;
+            num.Should().Be(4);
+        }
+    }
+}
