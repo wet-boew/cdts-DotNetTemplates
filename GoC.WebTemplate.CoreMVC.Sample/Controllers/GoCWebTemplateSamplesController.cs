68a5250cfc6a0385cad0e07bff12462b66bc5c95
Shaun Laughland <shaun.laughland@hrsdc-rhdcc.gc.ca>
2019-10-31 12:52:12 -0400

    adding feeback and share sample page works on #322

--- GoC.WebTemplate.CoreMVC.Sample/Controllers/GoCWebTemplateSamplesController.cs
+++ GoC.WebTemplate.CoreMVC.Sample/Controllers/GoCWebTemplateSamplesController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using GoC.WebTemplate.Components.Core.Services;
+using GoC.WebTemplate.Components.Entities;
 using GoC.WebTemplate.CoreMVC.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -61,6 +62,47 @@ namespace GoC.WebTemplate.CoreMVC.Sample.Controllers
 
         public IActionResult UnilingualErrorSample()
         {
+            return View();
+        }
+
+        public IActionResult BreadcrumbSample()
+        {
+            //Specify your breadcrumbs
+            WebTemplateModel.Breadcrumbs.Add(new Breadcrumb { Href = "http://www.canada.ca/en/index.html", Title = "Home" });
+            WebTemplateModel.Breadcrumbs.Add(new Breadcrumb { Href = "http://www.esdc.gc.ca/en/jobs/opportunities/index.page", Title = "Jobs" });
+            WebTemplateModel.Breadcrumbs.Add(new Breadcrumb { Href = "http://www.esdc.gc.ca/en/jobs/opportunities/youth_students.page", Title = "Opportunities" });
+            //Leaving the "href" parameter empty, will create the breadcrumb in text and not as a hyperlink. Useful for the last item of the breadcrumb list. 
+            WebTemplateModel.Breadcrumbs.Add(new Breadcrumb { Title = "FSWEP", Acronym = "Federal Student Work Experience Program" });
+
+            //Note: For your solution, the values should be coming from your culture sensitive source ex: resource files, db etc...)
+            return View();
+        }
+
+        public IActionResult FeedbackandShareThisPageSample()
+        {
+            //Display the FeedbackLink
+            WebTemplateModel.FeedbackLink.Show = true; //this could be set in the appsettings.json, key = "GoC.WebTemplate.showFeedbackLink"
+            WebTemplateModel.FeedbackLink.Url = "http://www.aircanada.com/en/customercare/customersolutions.html";
+            WebTemplateModel.FeedbackLink.UrlFr = "http://www.aircanada.com/fr/customercare/customersolutions.html"; //will be used if the CurrentUICulture is set to 'fr' / if not set, will assume FeedbackLinkUrl is bilingual
+
+
+            ////Specify the Share This Page with Media sites.
+            WebTemplateModel.Settings.ShowSharePageLink = true; //this could be set in the appsettings.json, key = "GoC.WebTemplate.showSharePageLink"
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.bitly);
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.blogger);
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.digg);
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.diigo);
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.email);
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.facebook);
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.gmail);
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.linkedin);
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.myspace);
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.pinterest);
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.reddit);
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.tumblr);
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.twitter);
+            WebTemplateModel.SharePageMediaSites.Add(SocialMediaSites.yahoomail);
+
             return View();
         }
     }
