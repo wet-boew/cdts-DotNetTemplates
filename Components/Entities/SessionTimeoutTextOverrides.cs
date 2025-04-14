3dbd763fa6f6e8a82d0965b36f4ad62aae49dae5
ahmad.shahid <ahmad.shahid@hrsdc-rhdcc.gc.ca>
2025-04-14 10:45:29 -0400

    Adding signInUrl and textOverrides property for session timeout

---
+++ Components/Entities/SessionTimeoutTextOverrides.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace GoC.WebTemplate.Components.Entities
+{
+    public class SessionTimeoutTextOverrides
+    {
+        /// <summary>
+        /// Text for the Continue Session button.
+        /// </summary>
+        [JsonProperty(PropertyName = "buttonContinue")]
+        public string ButtonContinue { get; set; }
+
+        /// <summary>
+        /// Text for the End Session button.
+        /// </summary>
+        [JsonProperty(PropertyName = "buttonEnd")]
+        public string ButtonEnd { get; set; }
+
+        /// <summary>
+        /// Text for the Sign In button
+        /// </summary>
+        [JsonProperty(PropertyName = "buttonSignin")]
+        public string ButtonSignIn { get; set; }
+
+        /// <summary>
+        /// Text for the message displayed below the timer.
+        /// </summary>
+        [JsonProperty(PropertyName = "timeoutEnd")]
+        public string TimeoutEnd { get; set; }
+
+        /// <summary>
+        /// Text for the message displayed when the session has expired.
+        /// </summary>
+        [JsonProperty(PropertyName = "timeoutAlready")]
+        public string TimeoutAlready { get; set; }
+    }
+}
